5a1d5f2d600092446ecc7037637e5720b17a1150
SaadEssam <SaadEssam059@gmail.com>
2022-09-18 16:27:04 +0200

    Admin Conf.g

--- store/models.py
+++ store/models.py
@@ -22,9 +22,10 @@ class Product(models.Model):
   author = models.CharField(max_length=255, default='admin')
   description = models.TextField(blank=True)
   image = models.ImageField(upload_to = 'image/')
+  slug = models.SlugField(max_length=255)
   price = models.DecimalField(max_digits=4, decimal_places=2)
   in_stock = models.BooleanField(default=True)
-  in_active = models.BooleanField(default=True)
+  is_active = models.BooleanField(default=True)
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
   
