0147f4c0ffd952fd437733a664b55a849a0a3d8c
SaadEssam <SaadEssam059@gmail.com>
2022-09-19 19:26:44 +0200

    Single page view

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ app_name = 'store'
 
 urlpatterns = [
   path('', views.all_products, name='all_products'),  
+  path('item/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 # Create your views here.
@@ -11,3 +11,8 @@ def categories(request):
 def all_products(request):
   products = Product.objects.all()
   return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+  product = get_object_or_404(Product, slug=slug, in_stock=True)
+  return render(request, 'store/products/detail.html', {'product': product})
