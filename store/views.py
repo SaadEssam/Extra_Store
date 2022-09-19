4f2b9125e1511d669c1cb64d9469ea8b46de27e3
SaadEssam <SaadEssam059@gmail.com>
2022-09-19 18:50:32 +0200

    Home page template

--- store/views.py
+++ store/views.py
@@ -3,6 +3,11 @@ from .models import Category, Product
 
 # Create your views here.
 
+def categories(request):
+  return {
+    'categories': Category.objects.all()
+  }
+
 def all_products(request):
   products = Product.objects.all()
   return render(request, 'store/home.html', {'products': products})
