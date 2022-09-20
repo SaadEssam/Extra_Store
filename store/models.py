3d5e29d60a8eed2f065b9ff77178e8c8cba51ce9
SaadEssam <SaadEssam059@gmail.com>
2022-09-20 14:07:01 +0200

    Dynamic Links

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -10,6 +11,9 @@ class Category(models.Model):
   class Meta:
     verbose_name_plural = 'categories'
     
+  def get_absolute_url(self):
+    return reverse('store:category_list', args=[self.slug])
+  
   def __str__(self):
     return self.name
   
@@ -34,6 +38,9 @@ class Product(models.Model):
     verbose_name_plural = 'Products'
     ordering = ('-created',)
     
+  def get_absolute_url(self):
+      return reverse('store:product_detail', args=[self.slug])
+  
     
   def __str__(self):
     return self.title
--- store/views.py
+++ store/views.py
@@ -16,3 +16,8 @@ def all_products(request):
 def product_detail(request, slug):
   product = get_object_or_404(Product, slug=slug, in_stock=True)
   return render(request, 'store/products/detail.html', {'product': product})
+
+def category_list(request, category_slug):
+  category = get_object_or_404(Category, slug=category_slug)
+  products = Product.objects.filter(category=category)
+  return render(request, 'store/products/category.html', {'category': category, 'products': products})
