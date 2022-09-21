bd56806e85d9c8002a12ff6c8f8330dcc045541a
SaadEssam <SaadEssam059@gmail.com>
2022-09-21 15:07:29 +0200

    Create custom object manager

--- store/models.py
+++ store/models.py
@@ -4,6 +4,11 @@ from django.urls import reverse
 
 # Create your models here.
 
+class ProductManager(models.Manager):
+  def get_queryset(self):
+    return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+
 class Category(models.Model):
   name = models.CharField(max_length=255, db_index=True)
   slug = models.SlugField(max_length=255, unique=True)
@@ -32,7 +37,8 @@ class Product(models.Model):
   is_active = models.BooleanField(default=True)
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
-  
+  objects = models.Manager()
+  products = ProductManager()
   
   class Meta:
     verbose_name_plural = 'Products'
--- store/views.py
+++ store/views.py
@@ -4,13 +4,8 @@ from .models import Category, Product
 
 # Create your views here.
 
-def categories(request):
-  return {
-    'categories': Category.objects.all()
-  }
-
 def all_products(request):
-  products = Product.objects.all()
+  products = Product.products.all()
   return render(request, 'store/home.html', {'products': products})
 
 
