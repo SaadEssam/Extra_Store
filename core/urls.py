166d3ed162771b896f9820466e5379e51d63db9c
SaadEssam <SaadEssam059@gmail.com>
2022-09-22 15:49:20 +0200

    Basket App

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('basket/', include('basket.urls', namespace='basket')),
     path('', include('store.urls', namespace='store')),
 ]
 
