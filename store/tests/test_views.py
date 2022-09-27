3f382af787a975b56eff2992b643d13d41af3ca8
SaadEssam <SaadEssam059@gmail.com>
2022-09-27 17:43:21 +0200

    building tests for basket

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,8 +1,10 @@
+from importlib import import_module
 from unittest import skip
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import HttpRequest
-from django.test import Client, RequestFactory, TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
@@ -17,7 +19,6 @@ class TestSkip(TestCase):
 class TestViewResponses(TestCase):
   def setUp(self):
     self.c = Client()
-    self.factory = RequestFactory()
     User.objects.create(username='Extra-admin')
     Category.objects.create(name='Programming', slug='Programming')
     self.data1 = Product.objects.create(category_id=1, title='Grokking Algorithm', created_by_id=1, 
@@ -48,16 +49,11 @@ class TestViewResponses(TestCase):
 
   def test_homepage_html(self):
     request = HttpRequest()
+    engine = import_module(settings.SESSION_ENGINE)
+    request.session = engine.SessionStore()
     response = all_products(request)
     html = response.content.decode('utf8')
     self.assertIn('<title>Extra Store</title>', html)
     self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
     self.assertTrue(response.status_code, 200)
 
-  def test_view_function(self):
-    request = self.factory.get('/grokking-algorithms')
-    response = all_products(request)
-    html = response.content.decode('utf8')
-    self.assertIn('<title>Extra Store</title>', html)
-    self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
-    self.assertTrue(response.status_code, 200)
