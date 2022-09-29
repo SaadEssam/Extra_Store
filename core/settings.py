5f458f2cef39880b388f5379435bec9a522627a9
SaadEssam <SaadEssam059@gmail.com>
2022-09-29 17:23:16 +0200

    user model

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'basket',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+# Custom user model
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = '/account/dashboard'
+LOGIN_URL = '/account/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- store/models.py
+++ store/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
@@ -24,7 +24,7 @@ class Category(models.Model):
 
 class Product(models.Model):
   category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
-  created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+  created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='product_creator')
   title = models.CharField(max_length=255)
   author = models.CharField(max_length=255, default='admin')
   description = models.TextField(blank=True)
