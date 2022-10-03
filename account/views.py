fdb5011a8f7d84b9625bed2502e98c748ac0b300
SaadEssam <SaadEssam059@gmail.com>
2022-10-03 16:48:32 +0200

    email & registration templates

--- account/views.py
+++ account/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 from .forms import RegistrationForm
+from .models import UserBase
+from .tokens import account_activation_token
 
 # Create your views here.
 
@@ -16,3 +23,17 @@ def account_register(request):
       user.set_password(registerForm.cleaned_data['password'])
       user.is_active = False
       user.save()
+      # Setup email
+      current_site = get_current_site(request)
+      subject = 'Activate your Account'
+      message = render_to_string('account/registration/account_activation_email.html', {
+        'user': user,
+        'domain': current_site.domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+      })
+      user.email_user(subject=subject, message=message)
+      
+  else:
+    registerForm = RegistrationForm()
+  return render(request, 'account/registration/register.html', {'form': registerForm})
