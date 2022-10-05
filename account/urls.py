089bb0a1b7e7a3cdb364d2d845c33b05cd37a148
SaadEssam <SaadEssam059@gmail.com>
2022-10-05 14:25:56 +0200

    Delete Account

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-
+from django.views.generic import TemplateView
 from .forms import (UserLoginForm)
 from . import views
 
@@ -14,4 +14,7 @@ urlpatterns = [
   path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
   path('dashboard/', views.dashboard, name='dashboard'),
   path('profile/edit/', views.edit_details, name='edit_details'),
+  path('profile/delete_user/', views.delete_user, name='delete_user'),
+  path('profile/delete_confirm/', TemplateView.as_view(template_name='account/user/delete_confirm.html'), 
+        name='delete_confirmation'),
 ]
--- account/views.py
+++ account/views.py
@@ -31,6 +31,15 @@ def edit_details(request):
   return render(request, 'account/user/edit_details.html', {'user_form': user_form})
 
 
+@login_required
+def delete_user(request):
+  user = UserBase.objects.get(user_name=request.user)
+  user.is_active = False
+  user.save()
+  logout(request)
+  return redirect('account:delete_confirmation')
+
+
 def account_register(request):
 
   if request.user.is_authenticated:
