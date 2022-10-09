6af6504732fcbbc0949ab463cbdd1fe4342329c2
SaadEssam <SaadEssam059@gmail.com>
2022-10-09 16:29:24 +0200

    connect orders to payment

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('basket/', include('basket.urls', namespace='basket')),
     path('payment/', include('payment.urls', namespace='payment')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('store.urls', namespace='store')),
     path('account/', include('account.urls', namespace='account')),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+
+from basket.basket import Basket
+from .models import Order, OrderItem
 
 # Create your views here.
+
+
+def add(request):
+  basket = Basket(request)
+  if request.POST.get('action') == 'post':
+    
+    user_id = request.user.id
+    order_key = request.POST.get('order_key')
+    baskettotal = basket.get_total_price()
+    
+    
+    if Order.objects.filter(order_key=order_key).exists():
+      pass
+    else:
+      order = Order.objects.create(user_id=user_id, full_name='name', address1='add1',
+                                    address2='add2', total_paid=baskettotal, order_key=order_key)
+      order_id = order.pk
+      for item in basket:
+        OrderItem.objects.create(order_id=order_id, product=item['product'], price=item['price'],
+                                  quantity=item['qty'])
+    response = JsonResponse({'success': 'Return something'})
+    return response
