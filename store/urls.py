f8547739c7ed8ef190ea9b12acfa48eb766f21ef
SaadEssam <SaadEssam059@gmail.com>
2022-10-10 17:23:54 +0200

    Refactoring store templates

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-  path('', views.all_products, name='all_products'),  
+  path('', views.all_products, name='store_home'),  
   path('<slug:slug>/', views.product_detail, name='product_detail'),
   path('store/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
