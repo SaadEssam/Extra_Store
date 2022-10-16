91ed6ecc4adbbe8243550c9496b262dcd5a7c20d
SaadEssam <SaadEssam059@gmail.com>
2022-10-16 19:17:25 +0200

    Building the database

--- store/models.py
+++ store/models.py
@@ -1,49 +1,107 @@
-from django.conf import settings
+from pydoc import describe
 from django.db import models
 from django.urls import reverse
-
-# Create your models here.
-
-class ProductManager(models.Manager):
-  def get_queryset(self):
-    return super(ProductManager, self).get_queryset().filter(is_active=True)
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
-  name = models.CharField(max_length=255, db_index=True)
-  slug = models.SlugField(max_length=255, unique=True)
+class Category(MPTTModel):
+  name = models.CharField(verbose_name=_("Category Name"), help_text=_("Required and unique"),
+                            max_length=255, unique=True)
+  slug = models.SlugField(verbose_name=_("Category safe URL"), max_length=255, unique=True)
+  parent = TreeForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+  is_active = models.BooleanField(default=True)
+
+  class MPTTMeta:
+    order_insertion_by = ["name"]
 
   class Meta:
-    verbose_name_plural = 'categories'
+    verbose_name = _("Category")
+    verbose_name_plural = _("Categories")
 
   def get_absolute_url(self):
-    return reverse('store:category_list', args=[self.slug])
+    return reverse("store:category_list", args=[self.slug])
 
   def __str__(self):
     return self.name
 
-class Product(models.Model):
-  category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
-  created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='product_creator')
-  title = models.CharField(max_length=255)
-  author = models.CharField(max_length=255, default='admin')
-  description = models.TextField(blank=True)
-  image = models.ImageField(upload_to='images/', default='images/default.png')
-  slug = models.SlugField(max_length=255)
-  price = models.DecimalField(max_digits=4, decimal_places=2)
-  in_stock = models.BooleanField(default=True)
+
+class ProductType(models.Model):
+  name = models.CharField(verbose_name=_("Product Name"), help_text=_("Required"), max_length=255, unique=True)
   is_active = models.BooleanField(default=True)
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  objects = models.Manager()
-  products = ProductManager()
 
   class Meta:
-    verbose_name_plural = 'Products'
-    ordering = ('-created',)
+    verbose_name = _("Product Type")
+    verbose_name_plural = _("Product Types")
+
+  def __str__(self):
+    return self.name
+
+
+class ProductSpecification(models.Model):
+  product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+  name = models.CharField(verbose_name=_("Name"), help_text=_("Required"), max_length=255)
+
+  class Meta:
+    verbose_name = _("Product Specification")
+    verbose_name_plural = _("Product Specifications")
+
+  def __str__(self):
+    return self.name
+
+
+class Product(models.Model):
+  product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+  category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+  title = models.CharField(verbose_name=_("title"), help_text=_("Required"), max_length=255)
+  description = models.TextField(verbose_name=_("description"), help_text=_("Not required"), blank=True)
+  slug = models.SlugField(max_length=255)
+  regular_price = models.DecimalField(verbose_name=_("Regular price"), help_text=_("Maximum 999.99"), 
+                                        error_messages={"name": {"max_length": _("The price must be between 0 and 999.99"),},}, 
+                                        max_digits=5, decimal_places=2)
+  discount_price = models.DecimalField(verbose_name=_("Discount price"), help_text=_("Maximum 999.99"), 
+                                        error_messages={"name": {"max_length": _("The price must be between 0 and 999.99"),},}, 
+                                        max_digits=5, decimal_places=2)
+  is_active = models.BooleanField(verbose_name=_("Product Visibility"), help_text=_("Change product Visibility"), default=True)
+  created_at = models.DateTimeField(_("Created at"), auto_now_add=True, editable=False)
+  updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+  class Meta:
+    ordering = ("-created_at",)
+    verbose_name = _("Product")
+    verbose_name_plural = _("Products")
 
   def get_absolute_url(self):
-    return reverse('store:product_detail', args=[self.slug])
+      return reverse("store:product_detail", args=[self.slug])
 
   def __str__(self):
     return self.title
+
+
+class ProductSpecificationValue(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  specification = models.ForeignKey(ProductSpecification, on_delete=models.RESTRICT)
+  value = models.CharField(verbose_name=_("value"), help_text=_("Product Specification value (maximum of 255 words)"), 
+                            max_length=255)
+
+  class Meta:
+    verbose_name = _("Product Specification Value")
+    verbose_name_plural = _("Product Specification values")
+
+  def __str__(self):
+    return self.value
+
+
+class ProductImage(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_image")
+  image = models.ImageField(verbose_name=_("image"), help_text=_("Upload a product image"), 
+                              upload_to="images/", default="images/default.png")
+  alt_text = models.CharField(verbose_name=_("Alternative text"), help_text=_("Please add alternative text"), 
+                                max_length=255, null=True, blank=True)
+  is_feature = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True, editable=False)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    verbose_name = _("Product Image")
+    verbose_name_plural = _("Product Images")
