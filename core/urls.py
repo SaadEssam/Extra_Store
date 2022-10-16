1d2a37cd0e2babcbe97967c7d649c66cb73369e0
SaadEssam <SaadEssam059@gmail.com>
2022-10-16 16:17:50 +0200

    Django_debug_toolbar Conf.

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -10,6 +11,7 @@ urlpatterns = [
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('store.urls', namespace='store')),
     path('account/', include('account.urls', namespace='account')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 
 
