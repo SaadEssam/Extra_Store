6206565134ecf049f5cb9f5267638b30bcae115d
SaadEssam <SaadEssam059@gmail.com>
2022-10-17 15:18:11 +0200

    Setting store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,21 +1,29 @@
-# from django.contrib import admin
-
-# from .models import Category, Product
-
-# # Register your models here.
-
-
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#   list_display = ['name', 'slug']
-#   prepopulated_fields = {'slug': ('name',)}
-
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#   list_display = ['title', 'author', 'slug', 'price',
-#                   'in_stock', 'created', 'updated']
-#   list_filter = ['in_stock', 'is_active']
-#   list_editable = ['price', 'in_stock']
-#   prepopulated_fields = {'slug': ('title',)}
+from django import forms
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import (Category, Product, ProductImage, ProductSpecification, ProductSpecificationValue, ProductType)
 
 
+admin.site.register(Category, MPTTModelAdmin)
+
+
+class ProductSpecificationInline(admin.TabularInline):
+  model = ProductSpecification
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+  inlines = [ProductSpecificationInline]
+
+
+class ProductImageInline(admin.TabularInline):
+  model = ProductImage
+
+
+class ProductSpecificationValueInline(admin.TabularInline):
+  model = ProductSpecificationValue
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+  inlines = [ProductSpecificationValueInline, ProductImageInline]
