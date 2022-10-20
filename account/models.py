3ccceb5ca08c5a3747dd7d69c32c1092f88ae367
SaadEssam <SaadEssam059@gmail.com>
2022-10-20 14:51:15 +0200

    change Account Table

--- account/models.py
+++ account/models.py
@@ -9,7 +9,7 @@ from django_countries.fields import CountryField
 
 class CustomAccountManager(BaseUserManager):
 
-  def create_superuser(self, email, user_name, password, **other_fields):
+  def create_superuser(self, email, name, password, **other_fields):
 
     other_fields.setdefault('is_staff', True)
     other_fields.setdefault('is_superuser', True)
@@ -20,34 +20,25 @@ class CustomAccountManager(BaseUserManager):
     if other_fields.get('is_superuser') is not True:
       raise ValueError('superuser must be assigned to is_superuser=True.')
 
-    return self.create_user(email, user_name, password, **other_fields)
+    return self.create_user(email, name, password, **other_fields)
 
-  def create_user(self, email, user_name, password, **other_fields):
+  def create_user(self, email, name, password, **other_fields):
 
     if not email:
       raise ValueError(_('You must provide an email address'))
 
     email = self.normalize_email(email)
-    user = self.model(email=email, user_name=user_name, **other_fields)
+    user = self.model(email=email, user_name=name, **other_fields)
     user.set_password(password)
     user.save()
     return user
 
 
 
-class UserBase(AbstractBaseUser, PermissionsMixin):
+class Customer(AbstractBaseUser, PermissionsMixin):
   email = models.EmailField(_('email address'), unique=True)
-  user_name = models.CharField(max_length=150, unique=True)
-  first_name = models.CharField(max_length=150, blank=True)
-  about = models.TextField(_('about'), max_length=500, blank=True)
-  # Delivery details
-  country = CountryField()
-  phone_number = models.CharField(max_length=15, blank=True)
-  postcode = models.CharField(max_length=12, blank=True)
-  address_line_1 = models.CharField(max_length=150, blank=True)
-  address_line_2 = models.CharField(max_length=150, blank=True)
-  town_city = models.CharField(max_length=150, blank=True)
-  # User Status
+  name = models.CharField(max_length=150)
+  mobile = models.CharField(max_length=20, blank=True)
   is_active = models.BooleanField(default=False)
   is_staff = models.BooleanField(default=False)
   created = models.DateTimeField(auto_now_add=True)
@@ -56,12 +47,12 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
   objects = CustomAccountManager()
 
   USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = ['user_name']
+  REQUIRED_FIELDS = ['name']
 
   class Meta:
     verbose_name = "Accounts"
     verbose_name_plural = "Accounts"
-    
+
   def email_user(self, subject, message):
     send_mail(
         subject,
@@ -72,4 +63,4 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
     )
 
   def __str__(self):
-    return self.user_name
+    return self.name
