b1e517f940e59c7c591e14cb89bfbf6f974cfb90
SaadEssam <SaadEssam059@gmail.com>
2022-10-23 18:48:45 +0200

    Complete Wishlist funtion

--- account/views.py
+++ account/views.py
@@ -1,13 +1,14 @@
+from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from orders.views import add, user_orders
+from orders.views import user_orders
 from store.models import Product
 
 from .forms import RegistrationForm, UserEditForm, UserAddressForm
@@ -135,15 +136,17 @@ def set_default(request, id):
 
 # Wishlist
 @login_required
+def wishlist(request):
+  products = Product.objects.filter(users_wishlist=request.user)
+  return render(request, "account/dashboard/user_wishlist.html", {"wishlist": products})
+
+@login_required
 def add_to_wishlist(request, id):
   product = get_object_or_404(Product, id=id)
   if product.users_wishlist.filter(id=request.user.id).exists():
     product.users_wishlist.remove(request.user)
+    messages.success(request, product.title + " has been removed from your Wishlist ")
   else:
     product.users_wishlist.add(request.user)
+    messages.success(request, "Added " + product.title + " to your Wishlist")
   return HttpResponseRedirect(request.META["HTTP_REFERER"])
-
-@login_required
-def wishlist(request):
-  products = Product.objects.filter(users_wishlist=request.user)
-  return render(request, "account/dashboard/user_wishlist.html", {"wishlist": products})
