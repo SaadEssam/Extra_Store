9302281cdc9214653dc00655539e88ed3e5343eb
SaadEssam <SaadEssam059@gmail.com>
2022-10-24 14:53:53 +0200

    checkout app | models

--- core/settings/base.py
+++ core/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'account',
     'orders',
     'mptt',
+    'checkout',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('basket/', include('basket.urls', namespace='basket')),
+    path('checkout/', include('checkout.urls', namespace='checkout')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('store.urls', namespace='store')),
     path('account/', include('account.urls', namespace='account')),
